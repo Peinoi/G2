f452c748a8ca845eca12a2bcc3bb85cfe7b82538
song <islbvv@gmail.com>
2025-11-11 23:02:39 +0900

    11월 11일 메인화면 수정

--- team_project/server/services/survey_service.js
+++ team_project/server/services/survey_service.js
@@ -1,12 +1,12 @@
-const mysql = require("../mappers/survey_mapper.js");
+const mysql = require('../mappers/survey_mapper.js');
 
 const findAll = async () => {
   try {
-    const list = await mysql.query("selectSurveyList");
+    const list = await mysql.query('selectSurveyList');
     return list;
   } catch (err) {
     console.log(err);
-    return { err: "DB조회 중 오류 발생" };
+    return { err: 'DB조회 중 오류 발생' };
   }
 };
 
